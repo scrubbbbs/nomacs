80c342aee821b7c086249dba49b3229d3db0c090
Scrubs <scrubbbbs@gmail.com>
2026-08-16 09:57:50 -0600

    testing: add KDSingleApplication to macos scripts

--- scripts/homebrew/init.sh
+++ scripts/homebrew/init.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
 # setup a ram disk for homebrew and compiling
-set -e 
+set -e
 diskutil eraseVolume HFS+ "Scratch" `hdiutil attach -nomount ram://$((2*4096*512))`
 
+# below part fails in brew install, I guess we just rm -rf /usr/local instead
+exit 0
+
+# mount ramdisk on top of /usr/local for a clean environment
 DISK=$(hdiutil attach -nomount ram://$((2*4096*512)) )
 sudo newfs_hfs -v Homebrew $DISK
 sudo mount -t hfs $DISK /usr/local
